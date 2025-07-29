1ef798ae6a9ef8cba68f81cf54f581a6a66b0f21
moraesadmg <moraesadmg@gmail.com>
2025-07-29 14:26:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -22,4 +22,8 @@ def executar():
     return jsonify({"erro": "Ação não reconhecida", "comando": comando_original})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
